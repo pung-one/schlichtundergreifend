01723c67fbccadf69f0ad36bf770a4d97c4a879a
pung-one <p-ungerer@systemli.org>
2024-06-18 18:28:24 +0200

    change ids

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import { Food } from "@/components/Food";
 import { Hero } from "@/components/Hero";
 import { NextDates } from "@/components/NextDates";
-import { PageContainer } from "@/components/PageContainer";
 import { Together } from "@/components/Together";
 
 export default function Home() {
