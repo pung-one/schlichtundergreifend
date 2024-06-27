7a01cfce97d318717356e6123e5d5d548ffd46d4
pung-one <p-ungerer@systemli.org>
2024-06-27 11:07:06 +0200

    add noindex metadata

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,18 @@ import { Food } from "@/components/Food";
 import { Hero } from "@/components/Hero";
 import { NextDates } from "@/components/NextDates";
 import { Together } from "@/components/Together";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  robots: {
+    index: false,
+    follow: false,
+    googleBot: {
+      index: false,
+      follow: false,
+    },
+  },
+};
 
 export default function Home() {
   return (
